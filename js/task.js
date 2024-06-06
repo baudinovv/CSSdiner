197aadccc811fa82d839841f6044fb3fa3bd3dcf
Alisher <alisher.baudinovv@gmail.com>
2024-06-06 22:51:33 +0500

    feat 2

--- js/task.js
+++ js/task.js
@@ -27,16 +27,18 @@ let levelsObj = {
             `,
         }
     ],
-}
-const lvlReduce = () => {    
-    arrowL.addEventListener( "click" , () => {
-        lvlCounter--;
-    });
-    arrowR.addEventListener( "click" , () => {
-        lvlCounter++;
-    });
+} 
+arrowL.addEventListener( "click" , () => {
+    lvlCounter--;
+    if (lvlCounter < 1) lvlCounter = 1;
+});
+arrowR.addEventListener( "click" , () => {
+    lvlCounter++;
+    if (lvlCounter > levelsObj.levels.length) lvlCounter = levelsObj.levels.length;
+});
+
+const lvlUpdate = () => {
     table.innerHTML = levelsObj.levels[lvlCounter - 1].tags;
     lvlTitle.innerHTML = `Level ${lvlCounter} of 32`;
-    console.log(lvlCounter);
-}
-setInterval(lvlReduce, 100);
+};
+setInterval(lvlUpdate, 100);
