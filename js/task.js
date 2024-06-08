d0176673701f6c6f4fa9e880709cb35be5aa9324
Alisher <alisher.baudinovv@gmail.com>
2024-06-08 19:25:17 +0500

    feat 3

--- js/task.js
+++ js/task.js
@@ -1,44 +1,64 @@
 let table = document.querySelector('#table');
-let arrowL = document.querySelector('#arrow-left')
-let arrowR = document.querySelector('#arrow-right')
-let lvlTitle = document.querySelector('#lvl-title')
-let lvlCounter = 1;
-let levelsObj = {
+let arrowL = document.querySelector('#arrow-left');
+let arrowR = document.querySelector('#arrow-right');
+let levelTitle = document.querySelector('#level-title');
+let taskTitle = document.querySelector('#task-title');
+let levelCounter = 1;
+
+let square =  document.createElement('div');
+let circle =  document.createElement('div');
+let aquaCircle =  document.createElement('div');
+square.className = 'square';
+circle.className = 'circle';
+aquaCircle.className = 'aqua-circle';
+
+let levelsObject = {
     levels : [
         {
-            lvl : 1,
-            tags : `
-             <div class="circle"><div class="square"><div class="aqua-circle"></div></div></div>
-            `, 
+            level : 1,
+            tags : [`circle`, circle, circle],
+            select: 'apples',
+            in: ['', '']
         },
         {
-            lvl : 2,
-            tags : `
-             <div class="circle"><div class="square"></div></div>
-             <div class="circle"><div class="square"></div></div>
-             <div class="circle"><div class="square"></div></div>
-            `,
+            level : 2,
+            tags : [circle , circle , circle],
+            select: 'bananas',
         },
         {
-            lvl : 3,
-            tags : `
-            <div class="circle"></div>
-            <div class="circle"></div>
-            `,
+            level : 3,
+            tags : [aquaCircle],
+            select: 'orange',
         }
     ],
-} 
+}
+
+// table.innerHTML = levelsObject.levels[levelCounter - 1].tags;
+levelTitle.innerHTML = `Level ${levelCounter} of 32`;
+taskTitle.innerHTML = `Select the ${levelsObject.levels[levelCounter - 1].select}`
+for(let el of levelsObject.levels[0].tags){
+    table.append(el);
+}
+
 arrowL.addEventListener( "click" , () => {
-    lvlCounter--;
-    if (lvlCounter < 1) lvlCounter = 1;
+    levelCounter--;
+    if (levelCounter < 1) levelCounter = 1;
+    levelUpdate();
 });
 arrowR.addEventListener( "click" , () => {
-    lvlCounter++;
-    if (lvlCounter > levelsObj.levels.length) lvlCounter = levelsObj.levels.length;
+    levelCounter++;
+    if (levelCounter > levelsObject.levels.length) levelCounter = levelsObject.levels.length;
+    levelUpdate();
 });
 
-const lvlUpdate = () => {
-    table.innerHTML = levelsObj.levels[lvlCounter - 1].tags;
-    lvlTitle.innerHTML = `Level ${lvlCounter} of 32`;
-};
-setInterval(lvlUpdate, 100);
+const levelUpdate = () => {
+    // table.innerHTML = levelsObject.levels[levelCounter - 1].tags;
+    table.innerHTML = '';
+
+    levelTitle.innerHTML = `Level ${levelCounter} of 32`;
+    taskTitle.innerHTML = `Select the ${levelsObject.levels[levelCounter - 1].select}`;
+    for(let el of levelsObject.levels[levelCounter - 1].tags){
+        table.append(el);
+    }
+    // for(let el of levelsObject.levels[levelCounter - 1].in){}
+};    
