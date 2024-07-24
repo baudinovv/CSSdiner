18335e6fe430a5df5f0d25f13aff41e85b96d55d
Alisher <alisher.baudinovv@gmail.com>
2024-07-24 23:38:27 +0500

    feat: lvls till 27

--- src/js/task.js
+++ src/js/task.js
@@ -4,8 +4,6 @@ import levelsObject from '../JSON/levels.json';
 
 let table = document.querySelector('#table');
 let tableLow = document.querySelector('#tableLow');
-let arrowL = document.querySelector('#arrow-left');
-let arrowR = document.querySelector('#arrow-right');
 let levelTitle = document.querySelector('#level-title');
 let taskTitle = document.querySelector('#task-title');
 let asideTitle = document.querySelector('#asideTitle');
@@ -13,92 +11,36 @@ let asideSubTitle = document.querySelector('#asideSubTitle');
 let asideSelector = document.querySelector('#asideSelector');
 let asideText = document.querySelector('#asideText');
 let asideExamples = document.querySelector('#asideExamples');
-
-// burger menu 
-
-let burgerMain = document.querySelector('#burger-content');
-let asideCheckbox = document.querySelector('#burger-checkbox');
-let burgerBox = document.querySelector('#burger-box');
-
-// variables for checking answer
-
 let htmlEditor = document.querySelector('#html-editor');
-let input = document.querySelector('.dev__css-editor__input-1');
-let btn = document.querySelector('.dev__css-editor__input-2');
 let checkBox = document.querySelector('#checkbox');
-let answer;
-
-//
-let editors = document.querySelector('#editors');
+let exampleTitle = document.querySelector('#exampleTitle')
 
 
-let levelCounter = 1;
+let arrowL = document.querySelector('#arrow-left');
+let arrowR = document.querySelector('#arrow-right');
 
-levelCounter = (localStorage.getItem('lastLevelUser')) ? localStorage.getItem('lastLevelUser') : 1;
+let input = document.querySelector('.dev__css-editor__input-1');
+
+let levelCounter = (localStorage.getItem('lastLevelUser')) ? localStorage.getItem('lastLevelUser') : 1;
 
 arrowL.addEventListener( "click" , () => {
     levelCounter--;
     if (levelCounter < 1) levelCounter = 1;
-    levelUpdate();
+    levelUpdate(levelCounter);
 });
 arrowR.addEventListener( "click" , () => {
     levelCounter++;
     if (levelCounter > levelsObject.levels.length) levelCounter = levelsObject.levels.length;
-    levelUpdate();
+    levelUpdate(levelCounter);
 });
-
-
-
-// checking answer
-const answerReduce = (answer) => {
-    let currentLevel = levelsObject.levels[levelCounter - 1];
-    return (answer) ? (Array.from(htmlEditor.querySelectorAll(`${answer}`))
-        .map((value) => value.hasAttributes('choosen')).filter(Boolean).length == currentLevel.answer 
-        && !Array.from(htmlEditor.querySelectorAll(`${answer}`))
-        .map((value) => value.hasAttributes('choosen')).includes(false)) ? true : false : 0;   
-}
-
-
-// getting value from input to check if it is right
-btn.addEventListener("click", () => {
-    let answer = input.value;
-    if(answerReduce(answer)){
-        localStorage.setItem(`level${levelCounter}`, 1);
-        arrowR.click();
-        input.value = '';
-    } else{ // shaking animation if answer is wrong
-        editors.style.animation = "shake 0.3s ease";
-        setTimeout(() => {editors.style.animation = "none";}, 300)
-    }
-});
-
-// click btn when user press enter 
-input.addEventListener('keypress', function(e){
-    if(e.which === 13){
-        e.preventDefault();
-        btn.click();
-    }
-});
-
 
 // checkbox is green when lvl is complete
-const levelDone = () => {
+const levelDone = (levelCounter) => {
     (localStorage.getItem(`level${levelCounter}`) == 1) ?
         checkBox.style.filter = "brightness(0) saturate(100%) invert(39%) sepia(89%) saturate(1196%) hue-rotate(67deg) brightness(98%) contrast(101%)"
     : checkBox.style.filter = "none";
 }
 
-const burgerLvlCheck = (level) => { // checkbox is green when lvl is complete in aside menu
-    return localStorage.getItem(`level${level}`) ? 
-        "filter: brightness(0) saturate(100%) invert(39%) sepia(89%) saturate(1196%) hue-rotate(67deg) brightness(98%) contrast(101%)"
-    : "filter: none";
-}
-
-import { burgerReduce } from './burger-menu';
-
-export {burgerModal} // export for burger-menu.js file 
-export {burgerMain} // export for burger-menu.js file 
-export {burgerBox} // export for burger-menu.js file 
 
 const modalReduce = (nodeIn, mode) => { // creating mini-modal around table elements
     const modal = document.createElement('div');   
@@ -106,8 +48,8 @@ const modalReduce = (nodeIn, mode) => { // creating mini-modal around table elem
     nodeIn.addEventListener('mouseover', (event) => {
         event.stopPropagation();
         tableLow.append(modal); 
-        modal.innerHTML = `&lt;${nodeIn.className.replace(' choosen ', '').replace('small', 'class = "small"').replace('-fancy', ' id ="fancy"')}&gt;` // rewriting modal text
-        document.body.style.position = 'relative';
+        modal.innerHTML = `&lt;${nodeIn.className.replace('-', ' ').replace(' choosen ', '').replace('small', ' class = "small"').replace('fancy', 'id ="fancy"')}&gt;` // rewriting modal text
+        // document.body.style.position = 'relative';
         modal.style.left = `${nodeIn.getBoundingClientRect().x + nodeIn.getBoundingClientRect().width/4}px`; // position relative to e.target element
         modal.style.top = `${nodeIn.getBoundingClientRect().y - nodeIn.getBoundingClientRect().height/mode}px`; // position relative to e.target element
         nodeIn.setAttribute("modal", "true");
@@ -117,28 +59,7 @@ const modalReduce = (nodeIn, mode) => { // creating mini-modal around table elem
         (Array.from(tableLow.children).includes(modal)) ? tableLow.removeChild(modal) : 0;
     });
 }
-
-const burgerModal = () => { // creating burger menu 
-    if(asideCheckbox.hasAttribute('active')){
-        for(let i = 0; i < levelsObject.levels.length ; i++){
-            const burgerItem = document.createElement('div');
-            burgerItem.className = 'level__burger-box__item';
-            burgerItem.innerHTML = `<img src="./public/img/checkbox.svg" alt="" style="${burgerLvlCheck(i + 1)}"><span>${i + 1}</span>&emsp;${levelsObject.levels[i].asideSelector}`;
-            if(levelCounter == i + 1){
-                burgerItem.style.backgroundColor = "rgb(50, 50, 50)"; // currentlevel item bckgrnd in burger menu 
-            }
-            burgerItem.addEventListener('click', () => {
-                levelCounter = i + 1;
-                asideCheckbox.removeAttribute('active');
-                burgerReduce(false); // where burgerItem is clicked, burger menu is closed 
-                levelUpdate();
-            });
-            burgerBox.append(burgerItem);
-        }
-    } 
-}
-
-const levelUpdate = () => {
+const levelUpdate = (levelCounter) => {
     table.innerHTML = ''; 
     
     localStorage.setItem(`lastLevelUser`, levelCounter); 
@@ -148,7 +69,8 @@ const levelUpdate = () => {
     let prevNode = table;
     
     levelTitle.innerHTML = `Level ${levelCounter} of 32`;
-    taskTitle.innerHTML = `Select the ${currentLevel.select}`;
+    levelTitle.setAttribute('level', `${levelCounter}`);
+    taskTitle.innerHTML = `Select ${currentLevel.select}`;
     htmlEditor.innerHTML = currentLevel.HTMLeditor;
     
     asideTitle.innerHTML = currentLevel.asideTitle;
@@ -157,31 +79,69 @@ const levelUpdate = () => {
     asideText.innerHTML = currentLevel.asideText;
     asideExamples.innerHTML = currentLevel.asideExamples;
 
-    levelDone();
+    input.value = "";
+    (currentLevel.example) ? exampleTitle.innerHTML = "Examples" : exampleTitle.innerHTML = "";
+
+
+    levelDone(levelCounter);
 
     for(let i = 0; i < currentLevel.tags[0].length; i++){
         let surface = table;
+        let pastNodeIn;
         for(let j = 0; j < currentLevel.tags.length; j++){
             if(!currentLevel.tags[j][i]) continue;
 
             const nodeIn = document.createElement('div');
-            
-            if(j > 1) nodeIn.style.top = `${-j + 6}0px`;
+            const nodeTags = currentLevel.tags;           
+            if(j > 1) 
+                nodeIn.style.top = `${-j + 6}0px`;
             if(j == 0){
-                if(currentLevel.tags[j][i].includes('choosen')) nodeIn.style.animation = "anti-chooseme 1s linear infinite";
-                if(currentLevel.tags[0][i].includes("bento") || currentLevel.tags[0][i].includes("plate") || currentLevel.tags[0][i].includes("plate-fancy")){
+                pastNodeIn = nodeIn;
+                if(nodeTags[j][i].includes('choosen')) nodeIn.style.animation = "anti-chooseme 1s linear infinite";
+                if(nodeTags[0][i].includes("bento") || nodeTags[0][i].includes("plate") || nodeTags[0][i].includes("plate-fancy")){
                     nodeIn.classList = currentLevel.tags[j][i];
                     table.append(nodeIn);
                     surface = nodeIn;
                     modalReduce(nodeIn, 3);
+                    nodeIn.style.zIndex = "2";
+                    // if(nodeTags[0][i].includes('for=')){
+                    //     const nodeFor = document.createElement('div');
+                    //     nodeFor.classList = "task__picture-tablemodals__for";
+                    //     nodeFor.style.left = `${nodeIn.getBoundingClientRect().x - nodeIn.offsetWidth*1/2}px`
+                    //     tableLow.append(nodeFor);
+                    //     console.log(nodeIn.getBoundingClientRect().x)
+                    // }
                     continue;
                 }
+                if(nodeTags[j][i].includes('apple')) nodeIn.style.position = "relative";
             }
-            (currentLevel.tags[j][i].includes('small')) ? modalReduce(nodeIn , 0.5) : modalReduce(nodeIn , 1);
-            nodeIn.classList = currentLevel.tags[j][i];   
+            if(currentLevel.tags[j][i].includes('bento')){
+                nodeIn.style.width = "65px";
+                nodeIn.style.height = "65px";
+                nodeIn.style.position = "absolute";
+                modalReduce(nodeIn , 2)
+                nodeIn.classList = currentLevel.tags[j][i];   
+                surface.append(nodeIn);
+                continue;
+            }
+            if(nodeTags[j][i].includes('for')){
+                nodeIn.classList = "task__picture-for";
+                nodeIn.style.outline = "none";
+                nodeIn.style.top = "50px";
+                nodeIn.style.width = "60px";
+                nodeIn.style.left = `${(table.getBoundingClientRect().left) }px`;
+                nodeIn.textContent = `${nodeTags[j][i].slice(nodeTags[j][i].indexOf(`'`) + 1, nodeTags[j][i].length - 1)}`;
+                nodeIn.style.transformOrigin = "right";
+                nodeIn.style.transform = "rotate(10deg)";
+                table.append(nodeIn);
+                console.log(pastNodeIn.getBoundingClientRect().x)
+                continue;
+            }
+            (nodeTags[j][i].includes('small')) ? modalReduce(nodeIn , 0.5) : modalReduce(nodeIn , 1);
+            nodeIn.classList = nodeTags[j][i];   
             surface.append(nodeIn);
         } 
     }
 };
-levelUpdate();
-levelDone();
+levelUpdate(levelCounter);
+export {levelUpdate}
